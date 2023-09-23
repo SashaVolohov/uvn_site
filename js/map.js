e63cb5cc51f0de40e74cb6b3aaee94f30d6a9371
Giovanni <rezcjikov@mail.ru>
2023-09-23 13:37:55 +0300

    fix: second fix of geojson fetching

--- js/map.js
+++ js/map.js
@@ -2,42 +2,53 @@ let map;
 let countries_list;
 
 function MapInit() {
-    if (this.status == 200 && this.responseText != null) {
-		let get_map = this.responseText;
+		if (this.status == 200 && this.responseText != null) {
+				let get_map = this.responseText;
 
-		countries_list = JSON.parse(get_map);
-	
-		let i = 0;
-	
-		while (countries_list[i]) {
+				countries_list = JSON.parse(get_map);
 
-			L.geoJSON(fetch(`../geo/${countries_list[i].id}.geojson`).then(response => response.json()), {
-				style: function () {
-					return {color: countries_list[i].color};
+				let i = 0;
+
+				while (i < countries_list.length) {
+						let countryId = countries_list[i].id;
+						let countryColor = countries_list[i].color;
+						
+						fetch(`../geo/${countryId}.geojson`)
+								.then(response => {
+										if (response.ok) {
+												return response.json();
+										} else {
+												throw new Error("Error: " + response.status);
+										}
+								})
+								.then(data => {
+										L.geoJSON(data, {
+												style: function () {
+														return { color: countryColor };
+												}
+										}).bindPopup(function (layer) {
+												return layer.feature.properties.name;
+										}).addTo(map);
+								})
+								.catch(error => {
+										console.error("Error:", error);
+								});
+
+						i++;
 				}
-			}).bindPopup(function (layer) {
-
-				return layer.feature.properties.name;
-			}).addTo(map);
-
-			i++;
-	
 		}
-	}
 }
 
-window.onload = function() {
+window.onload = function () {
+		map = L.map("map").setView([49.24, 17.94], 5);
 
-	map = L.map("map").setView([49.24, 17.94], 5);
+		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				maxZoom: 19,
+				attribution: "© OpenStreetMap"
+		}).addTo(map);
 
-	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		maxZoom: 19,
-		attribution: "© OpenStreetMap"
-	}).addTo(map);
-		
-	let get_map = new XMLHttpRequest();
-	get_map.onload = MapInit;
-	get_map.open("GET", "../map.json");
-	get_map.send();
-
+		let get_map = new XMLHttpRequest();
+		get_map.onload = MapInit;
+		get_map.open("GET", "../map.json");
+		get_map.send();
 }
